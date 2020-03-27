78ffacca4dad1f3233042a55ca47c2d9cfa474fc
mhs121 <muhammadhelmi.septiawan@gmail.com>
2020-03-27 11:20:03 +0800

    memanggil method dari class di package lain

--- main.java
+++ main.java
@@ -1,8 +1,14 @@
 import koneksi.database;
+import model.dosen;
+import model.mahasiswa;
+import model.matakuliah;
 
 public class Main{
 	public static void main(String [] args){
 		System.out.println("Ini program main");
 		database.hubungkan();
+		dosen.tampilanInfo();
+		mahasiswa.tampilanInfo();
+		matakuliah.tampilanInfo();
 	}
 }
---
+++ model/mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen{
+	public static void tampilanInfo(){
+		System.out.println();
+		System.out.println("===== Mahasiswa ================");
+		System.out.println("NPM :18630344");
+		System.out.println("Nama : Muhammad Helmi Septiawan");
+		System.out.println("================================");
+	}
+}
