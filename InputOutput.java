0ab43245e6e6b9127b15633de9b60f356278610a
mhs121 <62454499+mhs121@users.noreply.github.com>
2020-03-27 11:42:58 +0800

    Buat Input Sting ke Variable

--- InputOutput.java
+++ InputOutput.java
@@ -2,5 +2,8 @@ import java.until.scanner;
 public class InputOutput{
 	public static void main(String [] args){
 		scanner scanner = new scanner(System.in);
+		String nama;
+		System.out.print("masukan nama anda : ");
+		nama = scanner.nextLine();
 	}
 }
